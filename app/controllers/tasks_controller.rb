9c1a70cc2e0df9c14781a77fbfdbc823b3995d97
Leonardt Lauenstein <leonardt@outlook.com>
2020-02-18 16:43:04 +0000

    70%done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,43 @@ class TasksController < ApplicationController
    @tasks = Task.all
   end
 
-  def details
+  def show
     @task = Task.find(params[:id])
+  end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.create(check_params_create)
+
+    redirect_to task_path(task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(check_params_update)
+    redirect_to task_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def check_params_create
+    params.require(:task).permit(:title, :details)
+  end
+
+  def check_params_update
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
